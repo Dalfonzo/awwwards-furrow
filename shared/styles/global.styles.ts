c86da2942ade5801f0742402be51dff67eec1f84
David Pérez <dalfonzo18@gmail.com>
2022-06-26 15:55:43 -0400

    Changes fonts

--- shared/styles/global.styles.ts
+++ shared/styles/global.styles.ts
@@ -3,33 +3,33 @@ import { createGlobalStyle } from 'styled-components'
 export default createGlobalStyle`
 
   @font-face {
-    font-family: 'Montserrat';
+    font-family: 'Calibre';
     font-weight:300;
-    src: url('/assets/fonts/Montserrat-Light.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Light.ttf') format('ttf');
   }
 
   @font-face {
-    font-family: 'Montserrat';
+    font-family: 'Calibre';
     font-weight:400;
-    src: url('/assets/fonts/Montserrat-Regular.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Regular.ttf') format('ttf');
   }
 
   @font-face {
-    font-family: 'Montserrat';
+    font-family: 'Calibre';
     font-weight:600;
-    src: url('/assets/fonts/Montserrat-SemiBold.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-SemiBold.ttf') format('ttf');
   }
 
   @font-face {
-    font-family: 'Montserrat';
+    font-family: 'Calibre';
     font-weight:800;
-    src: url('/assets/fonts/Montserrat-Light.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Bold.ttf') format('ttf');
   }
 
   @font-face {
-    font-family: 'Montserrat';
+    font-family: 'Calibre';
     font-weight:900;
-    src: url('/assets/fonts/Montserrat-Black.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Black.ttf') format('ttf');
   }
 
   * {
@@ -48,7 +48,7 @@ export default createGlobalStyle`
   html {
     box-sizing: border-box;
     font-size: 16px;
-    font-family:'Montserrat',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family:'Calibre',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   body {
