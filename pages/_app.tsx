dae8c7ff56791aceedc1bd4a7bd5dea899684ca9
David Pérez <dalfonzo18@gmail.com>
2022-07-07 17:27:31 -0400

    Adds providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { CursorStyleProvider } from '~/context/cursorStyleContext'
+import { CustomThemeProvider, useCustomTheme } from '~/context/themeContext'
+import LandingLayout from '~/layouts/LandingLayout'
 import GlobalStyles from '~/shared/styles/global.styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { theme } = useCustomTheme()
+
   return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
+    <CustomThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CursorStyleProvider>
+          <LandingLayout>
+            <>
+              <GlobalStyles />
+              <Component {...pageProps} />
+            </>
+          </LandingLayout>
+        </CursorStyleProvider>
+      </ThemeProvider>
+    </CustomThemeProvider>
   )
 }
 
