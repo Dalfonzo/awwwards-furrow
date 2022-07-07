07c4d63ccc4001b222dec36f77605becf57186ed
David Pérez <dalfonzo18@gmail.com>
2022-07-07 17:25:03 -0400

    Adds hero canvas section

---
+++ components/hero/Hero.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Canvas = styled.canvas`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`
+
+export const Container = styled.main`
+  position: relative;
+`
+
+export const Video = styled.video`
+  object-fit: cover;
+`
---
+++ components/hero/Hero.tsx
@@ -0,0 +1,58 @@
+import { MouseEventHandler, useEffect, useRef, useState } from 'react'
+import { useCursorStyle } from '~/context/cursorStyleContext'
+import { useCustomTheme } from '~/context/themeContext'
+import useWindowSize from '~/hooks/useWindowSize'
+import * as S from './Hero.styles'
+
+const Hero = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null)
+  const { height, width } = useWindowSize()
+  const { isDarkMode } = useCustomTheme()
+  const { setCursorToHoverStyle, setCursorToNormalStyle } = useCursorStyle()
+
+  const [, setMousePosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+
+    if (!canvas) return
+
+    ctxRef.current = canvas.getContext('2d')
+
+    if (!ctxRef.current) return
+
+    ctxRef.current.fillStyle = isDarkMode ? '#000' : '#FFF'
+    ctxRef.current.fillRect(0, 0, width, height)
+  }, [width, height, isDarkMode])
+
+  const onMouseMoveHandler: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    const { clientX: currentX, clientY: currentY } = event
+
+    if (!ctxRef.current) return
+
+    ctxRef.current.globalCompositeOperation = 'destination-out'
+    ctxRef.current.lineCap = 'round'
+    ctxRef.current.lineWidth = 120
+
+    setMousePosition((prevPosition) => {
+      if (ctxRef.current) {
+        ctxRef.current.beginPath()
+        ctxRef.current.moveTo(prevPosition.x || currentX, prevPosition.y || currentY)
+        ctxRef.current.lineTo(currentX, currentY)
+        ctxRef.current.stroke()
+      }
+
+      return { x: currentX, y: currentY }
+    })
+  }
+
+  return (
+    <S.Container onMouseLeave={setCursorToNormalStyle} onMouseEnter={setCursorToHoverStyle}>
+      <S.Video src="/assets/video/featured-video.mp4" autoPlay loop width={width} height={height} muted playsInline />
+      <S.Canvas width={width} height={height} ref={canvasRef} onMouseMove={onMouseMoveHandler} />
+    </S.Container>
+  )
+}
+
+export default Hero
