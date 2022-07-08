e25c2439d65ce580ba8fa5f4db4199976453a3c9
David Pérez <dalfonzo18@gmail.com>
2022-07-07 20:30:01 -0400

    Fixes wrong font format

--- shared/styles/global.styles.ts
+++ shared/styles/global.styles.ts
@@ -5,31 +5,31 @@ export default createGlobalStyle`
   @font-face {
     font-family: 'Calibre';
     font-weight:300;
-    src: url('/assets/fonts/Calibre-Light.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Light.ttf') format('truetype');
   }
 
   @font-face {
     font-family: 'Calibre';
     font-weight:400;
-    src: url('/assets/fonts/Calibre-Regular.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Regular.ttf') format('truetype');
   }
 
   @font-face {
     font-family: 'Calibre';
     font-weight:600;
-    src: url('/assets/fonts/Calibre-SemiBold.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-SemiBold.ttf') format('truetype');
   }
 
   @font-face {
     font-family: 'Calibre';
     font-weight:800;
-    src: url('/assets/fonts/Calibre-Bold.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Bold.ttf') format('truetype');
   }
 
   @font-face {
     font-family: 'Calibre';
     font-weight:900;
-    src: url('/assets/fonts/Calibre-Black.ttf') format('ttf');
+    src: url('/assets/fonts/Calibre-Black.ttf') format('truetype');
   }
 
   * {
@@ -44,11 +44,11 @@ export default createGlobalStyle`
   *::after {
     box-sizing: inherit;
   }
-
+  /* ,-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; */
   html {
     box-sizing: border-box;
     font-size: 16px;
-    font-family:'Calibre',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family:'Calibre';
   }
 
   body {
