34bc6c3ad74f4b3b67dc2158e2566a0f3a8ab3de
David Pérez <dalfonzo18@gmail.com>
2022-07-09 13:54:23 -0400

    Adds minor changes

--- shared/styles/common.styles.ts
+++ shared/styles/common.styles.ts
@@ -1,7 +1,26 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   max-width: ${(props) => props.theme.breakPoints.lg};
   margin: auto;
   padding: 0 32px;
+`
+
+interface InnerContainerI {
+  spaceAsPadding?: boolean
+  spaceAsMargin?: boolean
+}
+
+export const InnerContainer = styled.div<InnerContainerI>`
+  ${(props) =>
+    props.spaceAsPadding &&
+    css`
+      padding: 0 8.3%;
+    `}
+
+  ${(props) =>
+    props.spaceAsMargin &&
+    css`
+      margin: 0 8.3%;
+    `}
 `
