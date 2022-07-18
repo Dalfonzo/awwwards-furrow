fe3dece08d0bd1ae0a7e4ba5aeb34ce1051e56ea
David Pérez <dalfonzo18@gmail.com>
2022-07-18 11:54:39 -0400

    Changes elementPosition hook to accept external dependencies

--- hooks/useElementPosition.ts
+++ hooks/useElementPosition.ts
@@ -1,21 +1,35 @@
-import { RefObject, useEffect, useState } from 'react'
+import { RefObject, useEffect, useMemo, useState } from 'react'
 import { useLayoutDimensions } from '~/context/layoutDimensionsContext'
 
-export const useElementPosition = ({ elementRef }: { elementRef: RefObject<HTMLElement> }) => {
+export const useElementPosition = ({
+  elementRef,
+  deps = [],
+}: {
+  elementRef: RefObject<HTMLElement>
+  deps?: unknown[]
+}) => {
   const { height, width } = useLayoutDimensions()
   const [elementPosition, setElementPosition] = useState({ x: 0, y: 0, left: 0, top: 0 })
+  const dependencies = useMemo(() => JSON.stringify(deps), [deps])
 
   useEffect(() => {
     const element = elementRef.current
 
     if (!element) return
 
-    const x = element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.offsetWidth / 2
-    const y = element.getBoundingClientRect().top + document.documentElement.scrollTop + element.offsetHeight / 2
-    const left = element.getBoundingClientRect().left
-    const top = element.getBoundingClientRect().top
-    setElementPosition({ x, y, left, top })
-  }, [elementRef, height, width])
+    const getDimensions = () => {
+      const x = element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.offsetWidth / 2
+      const y = element.getBoundingClientRect().top + document.documentElement.scrollTop + element.offsetHeight / 2
+      const left = element.getBoundingClientRect().left
+      const top = element.getBoundingClientRect().top
+      setElementPosition({ x, y, left, top })
+    }
+
+    window.addEventListener('scroll', getDimensions)
+    getDimensions()
+
+    return () => window.removeEventListener('scroll', getDimensions)
+  }, [elementRef, height, width, dependencies])
 
   return { x: elementPosition.x, y: elementPosition.y, left: elementPosition.left, top: elementPosition.top }
 }
