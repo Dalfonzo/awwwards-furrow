55ad271811c8bcc35bdf2d20b33a7549b5562b27
David Pérez <dalfonzo18@gmail.com>
2022-07-18 11:56:43 -0400

    Minor change

--- layouts/LandingLayout.tsx
+++ layouts/LandingLayout.tsx
@@ -1,14 +1,17 @@
 import Cursor from '~/components/common/cursor/Cursor'
 import Header from '~/components/common/header/Header'
+import { useCursorStyle } from '~/context/cursorStyleContext'
 
 const LandingLayout: React.FC<{ children: JSX.Element }> = ({ children }) => {
+  const { setCursorStyle } = useCursorStyle()
+
   return (
-    <>
+    <div onMouseEnter={() => setCursorStyle('normal-accent')}>
       <Header asFooter={false} />
       <Cursor />
       {children}
       <Header asFooter={true} />
-    </>
+    </div>
   )
 }
 
